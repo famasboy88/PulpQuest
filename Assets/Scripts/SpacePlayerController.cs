1a8523c1daf9cc0c0eedb94dce377a713d54aae0
famasboy88 <famasboy88@gmail.com>
2017-08-16 17:36:22 +0800

    Added first test

--- Assets/Scripts/SpacePlayerController.cs
+++ Assets/Scripts/SpacePlayerController.cs
@@ -7,12 +7,16 @@ public class SpacePlayerController : MonoBehaviour {
 	private Rigidbody2D rb2d;
 	private bool direction;
 	public float speed;
+	private float degreesToAdd =0f;
+	private Vector3 birdRotation = new Vector3 (0f, 0f, 0f);
 
 	// Use this for initialization
 	void Start () {
+		rb2d = GetComponent <Rigidbody2D> ();
 		speed *= (Random.value > 0.5f) ? 1 : -1f;
 		direction = (speed > 0) ? true : false;
-		rb2d = GetComponent <Rigidbody2D> ();
+		degreesToAdd = (rb2d.velocity.y > 0)? 15f : -15f;
+
 	}
 	
 	// Update is called once per frame
@@ -20,8 +24,10 @@ public class SpacePlayerController : MonoBehaviour {
 		if(Input.GetMouseButtonDown (0)){
 			speed *= -1;
 			direction = !direction;
+			degreesToAdd *= -1f;
 		}
-
-		rb2d.velocity=new Vector2(0f,speed);
+		birdRotation = new Vector3 (0,0,Mathf.Clamp (birdRotation.z+degreesToAdd,-35f,35f));
+		transform.eulerAngles = birdRotation;
+		rb2d.velocity = new Vector2 (0f, speed);
 	}
 }
